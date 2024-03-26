16fb64eaafc812d6ac1647fe88e8c8b70749d4d9
fatma said <f.said@studenti.unibg.it>
2024-03-26 15:34:22 +0100

    first commit

---
+++ src/hello/hello.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		int x,y;
+		double res;
+		x=15;
+		y=3;
+		res=x/y;
+		System.out.print("le quotient de la division est"+res);		
+
+	}
+
+}
